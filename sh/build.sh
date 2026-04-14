d9573af17e073069d83743e255d173fda3b1e52b
tmPAi-9910 <tmpai.9910@hotmail.com>
2026-04-14 14:55:22 +0900

    Reorder pip install commands in build script

--- sh/build.sh
+++ sh/build.sh
@@ -2,5 +2,5 @@
 set -e
 
 python -m pip install --upgrade pip
-python -m pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
-python -m pip install --no-deps -r requirements.txt
+python -m pip install -r requirements.txt
+python -m pip install --no-deps "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
